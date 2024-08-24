5120dbf2598af9e5adea90f0afeaeff162f6da80
IbadovB <ibadovb@gmail.com>
2024-08-24 08:13:35 +0400

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-/*const boxButton = document.createElement("div")
-boxButton.classList.add("box")*/
 
-document.querySelector("body").style.margin = "50px" 
+const box = document.querySelector(".box")
+document.querySelector("body").style.margin = "50px"
 document.querySelector(".box").style.width = "100px"
 document.querySelector(".box").style.height = "100px"
 document.querySelector(".box").style.border = "2px, solid, black"
@@ -11,27 +10,25 @@ document.querySelector(".box").style.background = "green"
 
 
 
-document.querySelector(".box").addEventListener ("mouseover", mouseOver);
-document.querySelector(".box").addEventListener ("mouseout", mouseOut);
+
+
+document.querySelector(".box").addEventListener("mouseover", mouseOver);
+document.querySelector(".box").addEventListener("mouseout", mouseOut);
 
 function mouseOver() {
-    document.querySelector(".box").style.color = "red";
-    document.querySelector(".box").style.background = "blue";
-    document.querySelector(".box").innerHTML= "mouse is over"
-  }  
-  function mouseOut() {
-    document.querySelector(".box").style.color = "yellow";
-    document.querySelector(".box").style.background = "grey";
-     document.querySelector(".box").innerHTML = "mouse is out"
-  }
+  document.querySelector(".box").style.color = "red";
+  document.querySelector(".box").style.background = "blue";
+  document.querySelector(".box").innerHTML = "mouse is over"
+  const newBox = document.createElement("div")
+  newBox.innerText = "new text"
+  document.body.appendChild(newBox)
 
-/*document.querySelector(".box").style.
-    width =  "100 px",
-    height = "100 px",
-    border = "2px, solid, black",
-    margin = "10px 0 0 0",
-    background = "green";*/
+}
+
+function mouseOut() {
+  document.querySelector(".box").style.color = "yellow";
+  document.querySelector(".box").style.background = "grey";
+  document.querySelector(".box").innerHTML = "mouse is out"
+}
 
 
-/*document.querySelector(".box").style.height = "100px"
-document.querySelector(".box").style.border = "2px, solid, red"*/
